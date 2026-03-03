0f7f3655f5fbea58bc0beb345ee19faf91479c30
andersongeng <andersongranado28@gmail.com>
2026-03-03 19:52:50 -0400

    Add: RegisterView

--- inventory_management_api/apps/user/views.py
+++ inventory_management_api/apps/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from .serializers import RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    # Permitimos que cualquiera pueda acceder a este endpoint para registrarse
+    permission_classes = [permissions.AllowAny]
