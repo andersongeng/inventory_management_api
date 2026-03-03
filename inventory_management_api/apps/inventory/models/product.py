0655b858e9335555f2d546dcc532b4cc47b8a42f
andersongeng <andersongranado28@gmail.com>
2026-03-03 01:31:41 -0400

    Add: product module with Product model

---
+++ inventory_management_api/apps/inventory/models/product.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+from .category import Category
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    sku = models.CharField(max_length=50, unique=True)
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='products'
+    )
+    price = models.DecimalField(max_digits=16, decimal_places=2)
+    stock = models.PositiveIntegerField(db_default=0)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        null=True,
+        related_name='created_products'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(db_default=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.sku})"
