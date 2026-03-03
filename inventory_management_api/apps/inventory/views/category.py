bd8edd8f520bfb99a1a1db81c516980d4d85416b
andersongeng <andersongranado28@gmail.com>
2026-03-03 15:31:53 -0400

    Add: category view module

---
+++ inventory_management_api/apps/inventory/views/category.py
@@ -0,0 +1,8 @@
+from inventory.models import Category
+from inventory.serializers import CategorySerializer
+from rest_framework import viewsets
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet to list, retrieve, create, update, partial_update and destroy categories
+    """
