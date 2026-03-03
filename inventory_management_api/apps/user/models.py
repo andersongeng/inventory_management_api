bbe353e87d5ae820bbdce7d376bd14bfc22f8756
andersongeng <andersongranado28@gmail.com>
2026-03-03 00:18:33 -0400

    Add: phone field to User model

--- inventory_management_api/apps/user/models.py
+++ inventory_management_api/apps/user/models.py
@@ -4,4 +4,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    phone = models.CharField(max_length=15, blank=True, null=True, unique=True)
