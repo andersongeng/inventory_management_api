cc152ece4761a17ebca9b204467a566bef0f6159
andersongeng <andersongranado28@gmail.com>
2026-03-03 15:42:29 -0400

    Register: CategoryViewSet

--- inventory_management_api/apps/inventory/urls.py
+++ inventory_management_api/apps/inventory/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from inventory.views import ProductViewSet
+from inventory.views import ProductViewSet, CategoryViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r"products", ProductViewSet, basename="product")
+router.register(r"categories", CategoryViewSet, basename="category")
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('', include(router.urls)),
 ]
