f2bccdaade242cd4e6c31677da321cdb3b174882
andersongeng <andersongranado28@gmail.com>
2026-03-03 15:45:54 -0400

    Register: InventoryMovementViewSet

--- inventory_management_api/apps/inventory/urls.py
+++ inventory_management_api/apps/inventory/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from inventory.views import ProductViewSet, CategoryViewSet
+from inventory.views import ProductViewSet, CategoryViewSet, InventoryMovementViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r"products", ProductViewSet, basename="product")
 router.register(r"categories", CategoryViewSet, basename="category")
+router.register(r"inventory_movements", InventoryMovementViewSet, basename="inventory_movement")
 
 urlpatterns = [
     path('', include(router.urls)),
