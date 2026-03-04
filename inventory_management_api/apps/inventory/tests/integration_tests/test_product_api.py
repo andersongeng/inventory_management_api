74fe54d712325a56ae0663dfc84c4d5328411a2b
andersongeng <andersongranado28@gmail.com>
2026-03-04 18:58:47 -0400

    Add: test_create_duplicate sku and test_negative_price tests

--- inventory_management_api/apps/inventory/tests/integration_tests/test_product_api.py
+++ inventory_management_api/apps/inventory/tests/integration_tests/test_product_api.py
@@ -21,8 +21,27 @@ def test_create_product_api(auth_client, create_test_user):
 
     assert response.status_code == 201
     assert response.data['name'] == "Sierra Circular"
-    # Verify created_by attribute were asigned to the product
     assert response.data['created_by'] == create_test_user.id
 
     # 4. Verify if the new product exists on the database
     assert Product.objects.filter(sku="SIE001").exists()
+
+@pytest.mark.django_db
+def test_create_duplicate_sku(auth_client):
+    # Create first object
+    Product.objects.create(name="Original", sku="UNIQ123", price=10, stock=5)
+    
+    # Try create a second one with the same sku
+    payload = {"name": "Copia", "sku": "UNIQ123", "price": 5, "stock": 1}
+    response = auth_client.post('/api/products/', payload)
+    
+    assert response.status_code == 400
+    assert "sku" in response.data
+
+@pytest.mark.django_db
+def test_negative_price(auth_client):
+    payload = {"name": "Test", "sku": "TEST", "price": -10}
+
+    response = auth_client.post('/api/products/', payload)
+
+    assert response.status_code == 400
