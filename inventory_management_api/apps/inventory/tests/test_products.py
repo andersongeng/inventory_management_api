eb6a27267cf8d855ecde6759759967bcf79cbfc5
andersongeng <andersongranado28@gmail.com>
2026-03-04 17:39:12 -0400

    Add: test_create_product

---
+++ inventory_management_api/apps/inventory/tests/test_products.py
@@ -0,0 +1,20 @@
+import pytest
+from inventory.models import Product
+from decimal import Decimal
+
+@pytest.mark.django_db
+def test_create_product(create_test_user):
+    # 1. Create new product
+    new_product = Product.objects.create(
+        name = "Taladro",
+        sku = "TAL01",
+        price = 1.00,
+        stock = 10,
+        created_by = create_test_user
+    )
+    print(new_product.created_by_id)
+
+    # 2. Asserts
+    assert new_product.name == "Taladro"
+    assert new_product.created_by == create_test_user
+    assert new_product.price == Decimal("1.00")
