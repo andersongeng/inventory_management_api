ff29217a6d4848c5996fa9749a60a2e5a8c1b8e2
andersongeng <andersongranado28@gmail.com>
2026-03-03 23:15:30 -0400

    Add: test_user_login_returns_jwt

--- inventory_management_api/apps/user/tests.py
+++ inventory_management_api/apps/user/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
 
-# Create your tests here.
+User = get_user_model()
+
+@pytest.mark.django_db
+def test_user_login_returns_jwt(api_client):
+    # 1. Arrange (Preparar el escenario)
+    username = "testuser"
+    password = "securepassword123"
+    User.objects.create_user(username=username, password=password)
+
+    # 2. Act (Ejecutar la acción)
+    response = api_client.post('/api/login/', {
+        "username": username,
+        "password": password
+    }, format='json')
+
+    # 3. Assert (Verificar resultados)
+    assert response.status_code == 200
+    assert 'access' in response.data
+    assert 'refresh' in response.data
