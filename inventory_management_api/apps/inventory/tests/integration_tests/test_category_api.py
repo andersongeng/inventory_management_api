a908e80a27b720a8e7f99a0e63da85b87fc33849
andersongeng <andersongranado28@gmail.com>
2026-03-04 22:47:43 -0400

    Create: test_create_duplicate_category test

--- inventory_management_api/apps/inventory/tests/integration_tests/test_category_api.py
+++ inventory_management_api/apps/inventory/tests/integration_tests/test_category_api.py
@@ -16,3 +16,21 @@ def test_create_category_success(auth_client):
     assert response.status_code == 201
     assert response.data['name'] == "Herramientas Eléctricas"
     assert Category.objects.filter(name="Herramientas Eléctricas").exists()
+
+@pytest.mark.django_db
+def test_create_duplicate_category(auth_client):
+        """Test do not allow duplicate categories"""
+
+        # Arrange: Create a first category
+        Category.objects.create(name="Construcción")
+
+        # Arrange: Data to create a second category
+        url = '/api/categories/'
+        payload = {"name": "Construcción"}
+        
+        # Act: Send POST request
+        response = auth_client.post(url, payload)
+        
+        # Assert: Verify API response
+        assert response.status_code == 400
+        assert "name" in response.data # DRF devuelve error de campo único
