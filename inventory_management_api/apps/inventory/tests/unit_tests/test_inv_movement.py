7b289b6dcf2f25396fc857c74b50f787f9ca9b40
andersongeng <andersongranado28@gmail.com>
2026-03-04 23:40:49 -0400

    Create: test_movement_choices_validation test

--- inventory_management_api/apps/inventory/tests/unit_tests/test_inv_movement.py
+++ inventory_management_api/apps/inventory/tests/unit_tests/test_inv_movement.py
@@ -14,5 +14,21 @@ def test_movement_str_representation(create_test_user):
         type="IN",
         quantity=100
     )
+    # Assert: Verifying representation
     # representation should be something like: "IN - Screw (100)"
     assert str(movement) == f"IN - {product.name} (100)"
+
+def test_movement_choices_validation(create_test_user):
+    # Arrange: Create product
+    product = Product.objects.create(name="Tornillo", sku="TOR", price=1, created_by=create_test_user)
+    
+    # Arrange: Create product movement verifying the choices
+    movement = InventoryMovement(
+        product=product,
+        user=create_test_user,
+        type=InventoryMovement.MovementType.OUTBOUND,
+        quantity=5
+    )
+
+    # Assert: check the correct choice
+    assert movement.type == "OUT"
