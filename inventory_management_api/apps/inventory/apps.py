1ddfe1a6e3d50de126356597c53db51782da3427
andersongeng <andersongranado28@gmail.com>
2026-03-04 22:12:34 -0400

    Add: ready method on InventoryConfig to load signals

--- inventory_management_api/apps/inventory/apps.py
+++ inventory_management_api/apps/inventory/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class InventoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventory'
+
+    def ready(self):
+        # Import signals to be registered by Django
+        import inventory.signals
