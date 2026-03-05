dde6995b67df1a8c47c070a3ef8656a3f64ff000
andersongeng <andersongranado28@gmail.com>
2026-03-05 03:36:14 -0400

    Add: Information about every option of the Category ViewSet

--- inventory_management_api/apps/inventory/views/category.py
+++ inventory_management_api/apps/inventory/views/category.py
@@ -1,6 +1,13 @@
 from inventory.models import Category
 from inventory.serializers import CategorySerializer
 from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema, extend_schema_view
+
+@extend_schema_view(
+    list=extend_schema(description="Get a list of all categories."),
+    create=extend_schema(description="Create a new category. Name must be unique."),
+    retrieve=extend_schema(description="Get the detail of an specific category."),
+)
 
 class CategoryViewSet(viewsets.ModelViewSet):
     """
