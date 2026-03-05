d34c286b8ea8a017149e4dd3166968edd7d91972
andersongeng <andersongranado28@gmail.com>
2026-03-04 23:33:08 -0400

    Create: test_category_verbose_name_plural test

--- inventory_management_api/apps/inventory/tests/unit_tests/test_category.py
+++ inventory_management_api/apps/inventory/tests/unit_tests/test_category.py
@@ -1,8 +1,13 @@
 import pytest
 from inventory.models import Category
 
-@pytest.mark.django_db
+pytestmark = pytest.mark.django_db
+
 def test_category_creation():
     category = Category.objects.create(name="Construction")
     assert category.name == "Construction"
     assert str(category) == "Construction"
+
+def test_category_verbose_name_plural():
+    # Verifica que el Meta esté bien configurado (Categories)
+    assert str(Category._meta.verbose_name_plural) == "Categories"
