1b517649291a1799de3dc64fc2cb74f9a7cd7732
andersongeng <andersongranado28@gmail.com>
2026-03-05 02:58:32 -0400

    Add: DEFAULT SCHEMA for drf_espectacular on DRF

--- inventory_management_api/settings/base.py
+++ inventory_management_api/settings/base.py
@@ -155,6 +155,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 from datetime import timedelta
