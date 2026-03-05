3b2209ddf0870ca62faec70af3a7dfa963d154d2
andersongeng <andersongranado28@gmail.com>
2026-03-05 04:05:24 -0400

    Add: small pieces of information for every filter parameter

--- inventory_management_api/apps/inventory/filters/product.py
+++ inventory_management_api/apps/inventory/filters/product.py
@@ -2,11 +2,32 @@ from django_filters import rest_framework as filters
 from inventory.models import Product
 
 class ProductFilter(filters.FilterSet):
-    name = filters.CharFilter(lookup_expr='iexact')
+    name = filters.CharFilter(lookup_expr='icontains')
     sku = filters.CharFilter(lookup_expr='iexact')
-    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
-    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+    
+    # Price range
+    min_price = filters.NumberFilter(
+        field_name="price",
+        lookup_expr='gte',
+        label="Minimum price (Greater than or equal)"
+    )
+    max_price = filters.NumberFilter(
+        field_name="price",                             
+        lookup_expr='lte',
+        label="Maximum price (Less than or equal)"
+    )
+    # Stock Range
+    min_stock = filters.NumberFilter(
+        field_name="stock",
+        lookup_expr='gte',
+        label="Minimum stock level"
+    )
+    max_stock = filters.NumberFilter(
+        field_name="stock",
+        lookup_expr='lte',
+        label="Maximum stock level"
+    )
     
     class Meta:
         model = Product
-        fields = ['category', 'stock']
+        fields = ['category']
