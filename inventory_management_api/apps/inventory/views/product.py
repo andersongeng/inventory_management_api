0e99b8a742d40008130c7f9a3e43f43462c52af9
andersongeng <andersongranado28@gmail.com>
2026-03-05 03:51:01 -0400

    Translate: perform_create method comment

--- inventory_management_api/apps/inventory/views/product.py
+++ inventory_management_api/apps/inventory/views/product.py
@@ -14,5 +14,5 @@ class ProductViewSet(viewsets.ModelViewSet):
     filterset_class = ProductFilter
 
     def perform_create(self, serializer):
-        # Aquí le decimos: "Al guardar, inyecta el usuario actual en created_by"
+        # Asign the actual user that create the product to the field created_by
         serializer.save(created_by=self.request.user)
