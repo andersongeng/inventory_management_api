f5f5c04cb3e9c25d4ae28f6e2b1194439261f623
andersongeng <andersongranado28@gmail.com>
2026-03-04 23:34:39 -0400

    Translate: comments from spanish to english

--- inventory_management_api/apps/inventory/tests/unit_tests/test_product.py
+++ inventory_management_api/apps/inventory/tests/unit_tests/test_product.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 pytestmark = pytest.mark.django_db
 
 def test_create_product(create_test_user):
-    # 1. Create new product
+    # Arrange
     new_product = Product.objects.create(
         name = "Drill",
         sku = "DRI01",
@@ -13,9 +13,8 @@ def test_create_product(create_test_user):
         stock = 10,
         created_by = create_test_user
     )
-    print(new_product.created_by_id)
 
-    # 2. Asserts
+    # Asserts: Verify product on DB
     assert new_product.name == "Drill"
     assert new_product.created_by == create_test_user
     assert new_product.price == Decimal("1.00")
