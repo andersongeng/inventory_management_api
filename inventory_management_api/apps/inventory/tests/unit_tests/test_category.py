9df11a988922936fbbe4e7963f44fa69ddde7383
andersongeng <andersongranado28@gmail.com>
2026-03-04 23:35:36 -0400

    Comment: test_category_creation test

--- inventory_management_api/apps/inventory/tests/unit_tests/test_category.py
+++ inventory_management_api/apps/inventory/tests/unit_tests/test_category.py
@@ -4,10 +4,13 @@ from inventory.models import Category
 pytestmark = pytest.mark.django_db
 
 def test_category_creation():
+    # Arrange and act 
     category = Category.objects.create(name="Construction")
+
+    # Assert: Verify product on DB
     assert category.name == "Construction"
     assert str(category) == "Construction"
 
 def test_category_verbose_name_plural():
-    # Verifica que el Meta esté bien configurado (Categories)
+    # 
     assert str(Category._meta.verbose_name_plural) == "Categories"
